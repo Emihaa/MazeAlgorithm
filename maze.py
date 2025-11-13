bebd35979eef479ce8393aeddacbe5bab9ca2aa1
Emihaa <emihaanpaa@gmail.com>
2025-11-13 15:25:31 +0200

    more controls, step-by-step maze generation

--- maze.py
+++ maze.py
@@ -13,6 +13,8 @@ pygame.init()
 screen_width    = 500
 screen_height   = 500
 seed            = 9
+seed_i          = 0
+delay_time      = 50
 
 # Maze info
 rows        = 20
@@ -53,9 +55,14 @@ class Square: #instead of index could be point
         self._width = sq_width
         self._spot = spot
 
+def GenerateSeed():
+    random.seed(seed)
+    grid = random.sample(range(0, rows * columns), rows * columns)
+    return grid
 
 # calculate the initial maze
 def GenerateMaze():
+
     squares = []
     x = border/2
     for i in range(rows):
@@ -79,6 +86,7 @@ def GenerateMaze():
     return squares
 
 squares = GenerateMaze()
+grid    = GenerateSeed()
 
 def FloodFillRoom(x, y, list):
     sq = squares[x][y]
@@ -112,80 +120,134 @@ def FloodFill(square, list):
     
     return list
 
-random.seed(seed)
-grid = random.sample(range(0, rows * columns), rows * columns)
+
+def BuildMaze(sq):
+    (x, y) = sq._spot
+    if sq._wall == False and sq._room == False:
+        rooms = 0
+
+        # above
+        listA = []
+        if y > 0:
+            listA = FloodFillRoom(x, y - 1, listA)
+
+        if listA: # check if there is content inside the list
+            rooms += 1
+
+        # below
+        listB = []
+        if y < columns - 1:
+            listB = FloodFillRoom(x, y + 1, listB)
+        
+        # then we check if the list is empthy and if it shares same elements with listA
+        if listB and not (set(listA) & set(listB)):
+            rooms += 1
+
+        # right
+        listC = []
+        if x < rows - 1:
+            listC = FloodFillRoom(x + 1, y, listC)
+        
+        if listC and not (set(listA) & set(listC)) and not (set(listB) & set(listC)):
+            rooms += 1
+
+        # left
+        listD = []
+        if x > 0:
+            listD = FloodFillRoom(x - 1, y, listD)
+
+        if listD and not (set(listA) & set(listD)) and not (set(listB) & set(listD)) and not (set(listC) & set(listD)):
+            rooms += 1
+
+        if rooms == 2:
+            sq._color = sq_color2
+            sq._room = True
+
+
+# get square from the seed
+def PickSquare(i):
+    index = grid[i]
+    y = (int)(index / columns)
+    x = index - (y * rows)
+    sq = squares[x][y]
+    return sq
+
 
 # the actual game loop
 running = True
 paused = False
+
+delay = pygame.USEREVENT + 0
+pygame.time.set_timer(delay, delay_time)
+
+def KeyButtonEvents():
+    global running, paused, seed, squares, grid, seed_i, delay_time
+
+    if event.key == pygame.K_q:
+        running = False
+    if event.key == pygame.K_SPACE:
+        paused = not paused
+        if paused == True:
+            pygame.time.set_timer(delay, 0)
+        else:
+            pygame.time.set_timer(delay, delay_time)
+    if event.key == pygame.K_r:
+        seed_i = 0
+        squares.clear()
+        squares = GenerateMaze()
+        pygame.time.set_timer(delay, delay_time)
+    if event.key == pygame.K_UP:
+        seed_i = 0
+        seed += 1
+        squares.clear()
+        grid = GenerateSeed()
+        squares = GenerateMaze()
+        if paused == True:
+            paused = False
+            pygame.time.set_timer(delay, delay_time)
+    if event.key == pygame.K_DOWN:
+        seed_i = 0
+        seed -= 1
+        squares.clear()
+        grid = GenerateSeed()
+        squares = GenerateMaze()
+        if paused == True:
+            paused = False
+            pygame.time.set_timer(delay, delay_time)
+    if event.key == pygame.K_RIGHT:
+        if (delay_time > 30):
+            delay_time -= 30
+            pygame.time.set_timer(delay, delay_time)
+        print(delay_time)
+    if event.key == pygame.K_LEFT:
+        if (delay_time < 3000):
+            delay_time += 30
+            pygame.time.set_timer(delay, delay_time)
+        print(delay_time)
+
 while running:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        # button events
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_q:
-                running = False
-            if event.key == pygame.K_SPACE:
-                paused = not paused
-            if event.key == pygame.K_r:
-                squares.clear()
-                squares = GenerateMaze()
+           KeyButtonEvents()
+        if event.type == delay:
+            if seed_i < len(grid):
+                sq = PickSquare(seed_i)
+                BuildMaze(sq)
+                seed_i += 1
+            else:
+                print("done")
+                pygame.time.set_timer(delay, 0)
+                paused = True
 
     screen.fill(bg_color)
+    # draw the squares
     for row in squares:
         for sq in row:
             x,y = sq._pos
             pygame.draw.rect(screen, sq._color, [x, y, sq._width, sq._width])
-
-    if paused == False:
-        # randomly pick a square
-        for i in range(len(grid)):
-            index = grid[i]
-            y = (int)(index / columns)
-            x = index - (y * rows)
-            sq = squares[x][y]
-
-            if sq._wall == False and sq._room == False:
-                rooms = 0
-
-                # above
-                listA = []
-                if y > 0:
-                    listA = FloodFillRoom(x, y - 1, listA)
-
-                if listA: # check if there is content inside the list
-                    rooms += 1
-
-                # below
-                listB = []
-                if y < columns - 1:
-                    listB = FloodFillRoom(x, y + 1, listB)
-                
-                # then we check if the list is empthy and if it shares same elements with listA
-                if listB and not (set(listA) & set(listB)):
-                    rooms += 1
-
-                # right
-                listC = []
-                if x < rows - 1:
-                    listC = FloodFillRoom(x + 1, y, listC)
-                
-                if listC and not (set(listA) & set(listC)) and not (set(listB) & set(listC)):
-                    rooms += 1
-
-                # left
-                listD = []
-                if x > 0:
-                    listD = FloodFillRoom(x - 1, y, listD)
-
-                if listD and not (set(listA) & set(listD)) and not (set(listB) & set(listD)) and not (set(listC) & set(listD)):
-                    rooms += 1
-
-                if rooms == 2:
-                    sq._color = sq_color2
-                    sq._room = True
-        pause = True
 
     pygame.display.flip()
 
